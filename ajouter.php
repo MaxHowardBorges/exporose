c0b53bbba463ce290d87cdf72384aa7668584fd0
Loboss2206 <b.logan006@gmail.com>
2023-05-14 23:24:34 +0200

    mise en place de l'ajout

--- ajouter.php
+++ ajouter.php
@@ -1,6 +1,14 @@
 <?php
 require_once "bd.php";
 require_once "increment.php";
-require_once "table.php";
 $db = createDbConnection();
+$table = $_POST['table'];
+$columns = $_POST['columns'];
+$values = '';
+foreach ($_POST['value'] as $value)
+    $values .= $value . ',';
+$values = substr($values, 0, -1);
 InitialiserIncrement($table);
+mysqli_query($db, "INSERT INTO $table($columns) VALUES($values);");
+header("Location: table.php?table=" . $table);
+exit();
--- table.php
+++ table.php
@@ -26,10 +26,13 @@
                     require_once "bd.php";
                     $db = createDbConnection();
                     $queryColumns = mysqli_query($db, "SHOW COLUMNS FROM $table;");
+                    $columns = '';
                     while ($row = mysqli_fetch_assoc($queryColumns)) {
                         $column = $row['Field'];
                         echo '<th>' . $column . '</th>';
+                        $columns .= $column . ',';
                     }
+                    $columns = substr($columns, strpos($columns, ",") + 1, strlen($columns) - strpos($columns, ",") - 2);
                     ?>
                     <th></th>
                     <th></th>
@@ -50,30 +53,33 @@
                 ?>
                 <tr>
                     <td></td>
-                    <?php
-                    $queryColumns = mysqli_query($db, "SHOW COLUMNS FROM $table;");
-                    while ($row = mysqli_fetch_assoc($queryColumns)) {
-                        if ($row['Field'] == "id_" . $table) {
-                            continue;
-                        }
-                        $type = $row['Type'];
-                        if (substr($type, 0, 3) == "int") {
-                            echo '<td><input type="number"></td>';
-                        } else if (substr($type, 0, 7) == "tinyint") {
-                            echo '<td><input type="number" name="est_admin" min="0" max="1"></td>';
-                        } else if (substr($type, 0, 7) == "varchar") {
-                            if ($row['Field'] == "email") {
-                                echo '<td><input type="email" name="email"></td>';
-                            } else if ($row['Field'] == "mot_de_passe") {
-                                echo '<td class="password-container"><input type="password" name="mot_de_passe" id="password"><i class="fa-solid fa-eye" id="eye"></i></td>';
-                            } else {
-                                echo '<td><input type="text"></td>';
+                    <form action="ajouter.php" method="POST">
+                        <?php
+                        $queryColumns = mysqli_query($db, "SHOW COLUMNS FROM $table;");
+                        while ($row = mysqli_fetch_assoc($queryColumns)) {
+                            if ($row['Field'] == "id_" . $table) {
+                                continue;
+                            }
+                            $type = $row['Type'];
+                            if (substr($type, 0, 3) == "int") {
+                                echo '<td><input type="number" name="value[]" required></td>';
+                            } else if (substr($type, 0, 7) == "tinyint") {
+                                echo '<td><input type="number" name="value[]" min="0" max="1" required></td>';
+                            } else if (substr($type, 0, 7) == "varchar") {
+                                if ($row['Field'] == "email") {
+                                    echo '<td><input type="email" name="value[]" required></td>';
+                                } else if ($row['Field'] == "mot_de_passe") {
+                                    echo '<td class="password-container"><input type="password" name="value[]" id="password" required><i class="fa-solid fa-eye" id="eye"></i></td>';
+                                } else {
+                                    echo '<td><input type="text" name="value[]  " required></td>';
+                                }
                             }
                         }
-                    }
-                    ?>
-                    <td><button id="ajout" onclick="ajouter.php">Ajouter</button></td>
-                    <td></td>
+                        echo '<input type="hidden" name="table" value=' . $table . '>';
+                        echo '<input type="hidden" name="columns" value=' . $columns . '>';
+                        ?>
+                        <td><button id="ajout" type="submit">Ajouter</button></td>
+                        <td></td>
                 </tr>
             </tbody>
         </table>
