45a521f3a6ab521597f8d0930b3d493f2d4a9b7a
Loboss2206 <b.logan006@gmail.com>
2023-05-15 20:26:49 +0200

    commentaires enleves

--- supprimer.php
+++ supprimer.php
@@ -5,8 +5,8 @@ $table = $_POST['table'];
 $column = $_POST['column'];
 $value = $_POST['value'];
 mysqli_query($db, "DELETE FROM $table WHERE $column = $value;");
-    // header("Location: table.php?table=" . $table);
-    // exit();
+header("Location: table.php?table=" . $table);
+exit();
 
     // require_once "bd.php";
     // $db = createDbConnection();
